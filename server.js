bd8536d1ff2b365ed61010c43dd36e32762beadc
sultanbaloch <61145760+sultanbaloch@users.noreply.github.com>
2020-12-17 18:42:41 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var server = express();
+
+server.get("/", (req, res, next) => {
+    console.log("some one get menu");
+    res.send("menu: what do you want? food or water");
+
+
+})
+
+server.listen(3000, () =>{
+    console.log("server is running port 3000");
+})
